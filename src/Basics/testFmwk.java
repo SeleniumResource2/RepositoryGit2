a220d392bf28908682d7eba6ceb237aae91c7224
SeleniumResource2 <selenium.resource2@gmail.com>
2018-07-25 10:30:34 +0530

    Updating in master

---
+++ src/Basics/testFmwk.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class testFmwk {
+	
+	public static void main (String[] args){
+		testFmwk();
+	}
+	
+	public static void testFmwk(){
+		System.out.println("First method in git");
+	}
+
+}
