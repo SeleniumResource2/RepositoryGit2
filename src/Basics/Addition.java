354896a499efb15226e5b2a920218deaf9d9e2a0
SeleniumResource2 <selenium.resource2@gmail.com>
2018-07-25 10:41:42 +0530

    Added new feature

---
+++ src/Basics/Addition.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class Addition {
+	
+	public static void main (String [] args){
+		addMethod();
+	}
+	
+	public static void addMethod(){
+		System.out.println("Addiition is done from the list");
+	}
+
+}
